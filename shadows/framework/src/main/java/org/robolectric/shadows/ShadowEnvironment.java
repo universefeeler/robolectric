79c15cd55e47745c7440d6f0b352fda2e31c37f6
Googler <noreply@google.com>
2019-03-06 17:34:08 -0800

    Use createIfNotExists() rather than if(!exists())create().
    PiperOrigin-RevId: 237153426

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowEnvironment.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowEnvironment.java
@@ -62,13 +62,13 @@ public class ShadowEnvironment {
 
   @Implementation
   protected static File getExternalStorageDirectory() {
-    if (!exists(EXTERNAL_CACHE_DIR)) EXTERNAL_CACHE_DIR = RuntimeEnvironment.getTempDirectory().create("external-cache");
+    EXTERNAL_CACHE_DIR = RuntimeEnvironment.getTempDirectory().createIfNotExists("external-cache");
     return EXTERNAL_CACHE_DIR.toFile();
   }
 
   @Implementation
   protected static File getExternalStoragePublicDirectory(String type) {
-    if (!exists(EXTERNAL_FILES_DIR)) EXTERNAL_FILES_DIR = RuntimeEnvironment.getTempDirectory().create("external-files");
+    EXTERNAL_FILES_DIR = RuntimeEnvironment.getTempDirectory().createIfNotExists("external-files");
     if (type == null) return EXTERNAL_FILES_DIR.toFile();
     Path path = EXTERNAL_FILES_DIR.resolve(type);
     try {
@@ -92,10 +92,6 @@ public class ShadowEnvironment {
     externalDirs.clear();
 
     sIsExternalStorageEmulated = false;
-  }
-
-  private static boolean exists(Path path) {
-    return path != null && Files.exists(path);
   }
 
   @Implementation
